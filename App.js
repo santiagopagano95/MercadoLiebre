64fc2126cb30da2521f59d7ddd425114da58808d
santiagopagano95 <santiagopagano95@gmail.com>
2022-09-26 13:40:13 -0300

    actualizacion app.js, package.json y agregar procfile

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.listen(3001,() =>{
+app.listen(process.env.PORT || 3001, function() {
     console.log('Servidor corriendo');
 });
 
